bcac1a0c45105a2f8c05e30219c7a798d21756ca
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-12 14:56:36 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+module "rg" {
+  source   = "./modules/resource-group"
+  rg_name  = var.resource_group_name
+  location = var.location
+}
+
+module "monitoring" {
+  source   = "./modules/monitoring"
+  rg_name  = module.rg.rg_name
+  location = var.location
+}
+
+module "acr" {
+  source   = "./modules/acr"
+  acr_name = var.acr_name
+  rg_name  = module.rg.rg_name
+  location = var.location
+}
+
+module "aks" {
+  source             = "./modules/aks"
+  cluster_name       = var.cluster_name
+  location           = var.location
+  rg_name            = module.rg.rg_name
+  node_count         = var.node_count
+  log_analytics_id   = module.monitoring.workspace_id
+  acr_id             = module.acr.acr_id
+}
