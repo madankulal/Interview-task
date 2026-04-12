eaf1552a9cb74e3e63a18900239f4a783e4500a8
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-12 14:55:15 +0530

    Create main.tf

---
+++ terraform/modules/aks/main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  dns_prefix          = "aksdemo"
+
+  default_node_pool {
+    name       = "nodepool"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+    type       = "VirtualMachineScaleSets"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = var.log_analytics_id
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = var.acr_id
+}
