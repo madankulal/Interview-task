905fd1242cff2493f2f4bbc86f9037f051b2c5c1
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-12 19:32:40 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Thanks for the opportunity and this app is running on AKS Cluster"
+    return "Thanks for the opportunity and This app is running on AKS Cluster"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
