ed04393de002db7a679fe9245a3077b089b0ac5d
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-13 07:18:59 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Thanks for the opportunity and This app is running on AKS Cluster."
+def home():
+    return os.getenv("APP_MESSAGE", "Default Message")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000)
