1d9a56c606922d0b0c98ac88014d2fef45ea071d
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-13 07:09:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,23 @@ module "aks" {
   log_analytics_id   = module.monitoring.workspace_id
   acr_id             = module.acr.acr_id
 }
+resource "azurerm_key_vault_access_policy" "aks_policy" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = var.tenant_id
+  object_id    = var.aks_object_id
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+}
+module "keyvault" {
+  source              = "./modules/keyvault"
+  kv_name             = "aks-devops-kv"
+  location            = var.location
+  resource_group_name = module.rg.rg_name
+  tenant_id           = var.tenant_id
+  app_message         = "Thanks for the opportunity and this app is running on AKS Cluster"
+
+  aks_object_id       = module.aks.kubelet_identity_object_id
+}
