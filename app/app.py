e20f5da5c335275b2742ad7d8e935a6959654d63
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-13 10:39:54 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,8 +3,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def get_message():
+
+    try:
+        with open("/mnt/secrets/APP-MESSAGE", "r") as f:
+            return f.read().strip()
+    except Exception:
+        pass
+    env_message = os.getenv("APP_MESSAGE")
+    if env_message:
+        return env_message
+    return "Default Message"
+
 @app.route("/")
 def home():
-    return os.getenv("APP_MESSAGE", "Default Message")
+    return get_message()
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
