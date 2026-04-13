4ac6c78a8446287a65226a05c4be6166bff48808
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-13 08:32:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,14 @@ module "aks" {
   acr_id             = module.acr.acr_id
 }
 
+module "keyvault" {
+  source              = "./modules/keyvault"
+  kv_name             = "aks-devops-kv"
+  location            = var.location
+  resource_group_name = module.rg.rg_name
+  tenant_id           = var.tenant_id
+  app_message         = "Thanks for the opportunity and this app is running on AKS Cluster"
+
+  aks_object_id       = module.aks.kubelet_identity_object_id
+}
+
