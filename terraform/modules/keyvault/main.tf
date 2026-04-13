7e5aa5242718388aa38068bb61a01f0dadbe6477
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-04-13 07:06:15 +0530

    Create main.tf

---
+++ terraform/modules/keyvault/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault" "kv" {
+  name                        = var.kv_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+}
+
+resource "azurerm_key_vault_secret" "app_secret" {
+  name         = "APP-MESSAGE"
+  value        = var.app_message
+  key_vault_id = azurerm_key_vault.kv.id
+}
